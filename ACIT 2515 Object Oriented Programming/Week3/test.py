06b834d6a5fc505455284ea46bf049af18a074a7
Markus Afonso <mafonso4@my.bcit.ca>
2023-01-27 13:00:04 -0800

    week4.5

--- ACIT 2515 Object Oriented Programming/Week3/test.py
+++ ACIT 2515 Object Oriented Programming/Week3/test.py
@@ -1,6 +1,48 @@
-class Example:
-  pass
+# # # class Example:
+# # #   pass
 
-quiz = Example()
+# # # quiz = Example()
 
-print(quiz)
+# # # print(quiz)
+# # class Parent:
+# #   def __init__(self):
+# #     self.number = 10
+
+# # class Child(Parent):
+# #   def __init__(self):
+# #     self.number = 40
+
+# # instance = Child()
+
+# # print(instance.number)
+
+
+# class Parent:
+#   def __init__(self):
+#     self.number = 10
+
+# class Child(Parent):
+#   def __init__(self):
+#     self.number = 40
+#     super().__init__()
+
+# instance = Child()
+
+# print(instance.number)
+
+# class Person:
+#   def __init__(self):
+#     self.name = "Tim"
+#     self.program = "CIT"
+
+#   def say_hello(self):
+#     print("Hello!")
+  
+# def main():
+#   main()
+
+# if __name__ == "__main__":
+#   tim = Person()
+#   print(tim)
+  
+  
--- ACIT 2515 Object Oriented Programming/Week4/BankLab/bank.py
+++ ACIT 2515 Object Oriented Programming/Week4/BankLab/bank.py
@@ -1,5 +1,5 @@
-from customer import Customer
-from account import *
+from account import Account, CreditAccount, SavingsAccount
+
 
 
 class Bank:
@@ -7,8 +7,8 @@ class Bank:
         self.name = name
         self.accounts = []
 
-        
     def create_account(self, category, owner, interest_rate=0):
+
         new_account = None
         if category not in ("account", "credit", "savings"):
             return AttributeError
@@ -20,24 +20,24 @@ class Bank:
             new_account = SavingsAccount(owner)
         self.accounts.append(new_account)
         return new_account
- 
-    def find_accounts_by_ssn(self, ssn):
-        a = []
+
+    def find_accounts_by_ssn(self, ssn) -> list:
+        account_list = []
         for acc in self.accounts:
             if acc.owner.ssn == ssn:
-                a.append(acc)
-        return a
-        
-    def find_accounts_by_name(self, name):
-        a = []
+                account_list.append(acc)
+        return account_list
+
+    def find_accounts_by_name(self, name) -> list:
+        account_list = []
         for acc in self.accounts:
             if acc.owner.name == name:
-                a.append(acc)
-        return a
-    
+                account_list.append(acc)
+        return account_list
+
     @property
     def balance(self):
-        total = 0 
+        total = 0
         for account in self.accounts:
             total += account.amount
         return total
